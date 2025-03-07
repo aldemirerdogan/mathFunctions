1ca7b1d140eeff8fa339f967231241d7536329c9
erdoganaldemir <aldemir.erdogan@hotmail.com>
2025-03-07 17:36:09 +0300

    Initial Commit

---
+++ mathFunctions/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  mathFunctions
+//
+//  Created by Erdoğan Aldemir on 7.03.2025.
+//
+
+// call header funcstions for predefined standards.
+#include <stdio.h>
+#include "math.h"
+#include "time.h"
+#include "stdlib.h"
+
+int main(int argc, const char * argv[]) {
+    // define variables
+    float logaritmic_value;
+    float logaritmic_e;
+    
+    srand((int)time(0));    // call seed value for real-time radomization
+    int deger = rand()%100;      // Returns a pseudo-random integer between 0 and RAND_MAX.
+
+    logaritmic_value = log10(deger);
+    logaritmic_e = log(deger);
+    // logarithm of x base b = log(x)/log(b)
+    
+    printf("Logarithm (base-10) of %d is %f \n", deger, logaritmic_value );
+    printf("Logarithm (base-E) of %d is %f \n", deger, logaritmic_e );
+
+    
+    return 0;
+}
